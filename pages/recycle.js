64e70753f7a63c7356b7d81499eec24a154460c1
Alex Grafen <alex.grafen@gmail.com>
2022-06-27 16:19:17 +0100

    create trial api

--- pages/recycle.js
+++ pages/recycle.js
@@ -1,3 +1,33 @@
+import data from "../data/n195sh"
+
+// console.log(data)
+
+
+
 export default function Recycle() {
-    return (<p>test</p>);
+    //create use state for search term
+    //entered text is matched against postcode we have (n195sh)
+    //generate list based on postcode or return an error message
+    fetch(``)
+        .then((response) => {
+            if (!response.ok) throw new Error(response.status);
+            console.log(response.json());
+            return response.json();
+        })
+
+
+    //    async function getData(postcode){
+
+    //          const data = await `www.fdjkkdjgjkdl${postcode}dkfjkdsfdkl`
+
+    //     }
+
+    return (
+        <>
+            <form>
+                <input type="search"></input>
+            </form>
+            <p>test</p>
+        </>
+    );
 }
