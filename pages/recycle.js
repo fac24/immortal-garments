3e349d1f399cd3ed3d262af17683a83378294850
Alex Grafen <alex.grafen@gmail.com>
2022-06-27 17:58:50 +0100

    work on api

--- pages/recycle.js
+++ pages/recycle.js
@@ -1,19 +1,43 @@
-import data from "../data/n195sh"
 
-// console.log(data)
+import React, { useState } from "react";
+
+export async function getServerSideProps() {
+    async function handleSearch() {
+        console.log(9)
+        const result = await fetch(`https://rl.recyclenow.com/widget/londonrecycles.co.uk/locations/SW15%203QG?limit=30&radius=25&materials%5B%5D=57&callback=jQuery35105193289468312947_1656342986710&_=1656342986717`)
+
+
+
+        // const result = await fetch('https://immortal-garments.vercel.app/api/n195sh', {
+        //     // method: "GET",
+        //     mode: 'no-cors',
+        //     // headers: { "Content-type": "application/json;charset=UTF-8" }
+        // });
+        // const result = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+        console.log(11)
+        console.log(result);
+        const newdata = await result;
+
+        console.log(14);
+        console.log(newdata)
+        setData(newdata);
+        console.log(data);
+    }
+
+}
+
 
 
 
 export default function Recycle() {
+    const [data, setData] = useState(null);
+
+
     //create use state for search term
     //entered text is matched against postcode we have (n195sh)
     //generate list based on postcode or return an error message
-    // fetch(``)
-    //     .then((response) => {
-    //         if (!response.ok) throw new Error(response.status);
-    //         console.log(response.json());
-    //         return response.json();
-    //     })
+
+
 
 
     //    async function getData(postcode){
@@ -24,8 +48,13 @@ export default function Recycle() {
 
     return (
         <>
-            <form>
-                <input type="search"></input>
+            <form onSubmit={e => {
+                e.preventDefault();
+
+            }}>
+                <label name="search">Enter your postcode...</label>
+                <input type="search" name="search"></input>
+                <button type="submit" onClick={handleSearch}>Submit</button>
             </form>
             <p>test</p>
         </>
