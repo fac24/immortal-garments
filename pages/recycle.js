663b938fd05cb30ba46d5886b44cb1f5810fd8ac
Alex Grafen <alex.grafen@gmail.com>
2022-06-27 16:21:25 +0100

    comment out section

--- pages/recycle.js
+++ pages/recycle.js
@@ -8,12 +8,12 @@ export default function Recycle() {
     //create use state for search term
     //entered text is matched against postcode we have (n195sh)
     //generate list based on postcode or return an error message
-    fetch(``)
-        .then((response) => {
-            if (!response.ok) throw new Error(response.status);
-            console.log(response.json());
-            return response.json();
-        })
+    // fetch(``)
+    //     .then((response) => {
+    //         if (!response.ok) throw new Error(response.status);
+    //         console.log(response.json());
+    //         return response.json();
+    //     })
 
 
     //    async function getData(postcode){
