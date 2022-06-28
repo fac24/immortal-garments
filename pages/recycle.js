9d934b30062d07752de4eb7f22c3036826703321
Maria <maria.louisa.castro@gmail.com>
2022-06-28 16:36:05 +0100

    Search with user input and create basic error handle Co-authored-by: Sonia <sonianb@users.noreply.github.com>

--- pages/recycle.js
+++ pages/recycle.js
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 
 export default function Recycle() {
     const [data, setData] = useState(null);
+    const [userInput, setUserInput] = useState("");
+    const [error, setError] = useState(null);
     let listCount = 7;
     
+    const onChange = event => setUserInput(event.target.value);
+    
     async function handleSearch() {
-        const result = await fetch(`api/n195sh`)
+        const result = await fetch(`api/${userInput.replace(/ /g,'')}`)
+
+        if(!result.ok) {
+            setData(null)
+            setError(`Oops, something went wrong: ${result.status}.`)
+            return;
+        }
+
+        setError(null)
         const newdata = await result.json();
         setData(newdata.items);
     }
@@ -14,13 +26,16 @@ export default function Recycle() {
         <>
             <form onSubmit={e => {
                 e.preventDefault();
-
             }}>
                 <label name="search">Enter your postcode...</label>
-                <input type="search" name="search"></input>
-                <button type="submit" onClick={handleSearch}>Submit</button>
+                <input type="search" name="search" onChange={onChange} value={userInput}></input>
+                <button 
+                type="submit" 
+                onClick={handleSearch}
+                >Submit</button>
             </form>
             <p>Find your nearest textile recycle point</p>
+            
 
         {data ? data.map((item, index) => { 
            if(index < listCount)
@@ -31,6 +46,8 @@ export default function Recycle() {
             <p>{item.distance}</p> 
             </div>
             )}): ""}
+
+        {error ? error : ""}
 
         </>
     );
