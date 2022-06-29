5c8c79073103a080140f60ebfdbf57c9071e9b0b
Maria <maria.louisa.castro@gmail.com>
2022-06-29 14:48:43 +0100

    Clear cypress tests

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,20 +1,8 @@
-describe('Navigation bar', () => {
-  it('checks the links on the nav bar', () => {
-    // Visit landing page
+// Will add e2e tests here for each uer journey on the site
+
+describe('Test', () => {
+  it('checks the test test files are linked', () => {
+    // Basic check to test config
     cy.visit('https://immortal-garments.vercel.app/')
-    // Click on the donate tab
-    cy.get('a[href*="donate"]').click()
-    // The new url should include "/donate"
-    cy.url().should('include', '/donate')
-    // The loaded page should contain an h1 with "something" ADD HERE
-    // Repeat for all nav links
-    cy.get('a[href*="recycle"]').click()
-    cy.url().should('include', '/recycle')
-    // The loaded page should contain an h1 with "something" ADD HERE
-    cy.get('a[href*="diy"]').click()
-    cy.url().should('include', '/diy')
-    cy.get('a[href*="tailors"]').click()
-    cy.url().should('include', '/tailors')
-    // The loaded page should contain an h1 with "something" ADD HERE
   })
 })
