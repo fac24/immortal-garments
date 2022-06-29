0c4126d2595e55bfa52a00a8db61a23a2a22fdd0
Alex Grafen <alex.grafen@gmail.com>
2022-06-29 14:30:13 +0100

    spelling and modularisation

--- pages/recycle.js
+++ pages/recycle.js
@@ -1,63 +1,55 @@
 import React, { useState } from "react";
+import SearchPostcode from "../components/Searchpostcode";
+
 
 export default function Recycle() {
-  const [data, setData] = useState(null);
-  const [userInput, setUserInput] = useState("");
-  const [error, setError] = useState(null);
-  let listCount = 7;
+    const [data, setData] = useState(null);
+    const [userInput, setUserInput] = useState("");
+    const [error, setError] = useState(null);
+    let listCount = 7;
 
-  const onChange = (event) => setUserInput(event.target.value);
+    const onChange = (event) => setUserInput(event.target.value);
 
-  async function handleSearch() {
-    const result = await fetch(`api/${userInput.replace(/ /g, "")}`);
+    async function handleSearch() {
+        const result = await fetch(`api/${userInput.replace(/ /g, "")}`);
+        console.log("reached handleSearch");
+        if (!result.ok) {
+            setData(null);
+            setError(`Oops, something went wrong: ${result.status}.`);
+            return;
+        }
 
-    if (!result.ok) {
-      setData(null);
-      setError(`Oops, something went wrong: ${result.status}.`);
-      return;
+        setError(null);
+        const newdata = await result.json();
+        setData(newdata.items);
     }
 
-    setError(null);
-    const newdata = await result.json();
-    setData(newdata.items);
-  }
+    return (
+        <>
+            <SearchPostcode
+                onChange={onChange}
+                value={userInput}
+                handleSearch={handleSearch}
+            />
 
-  return (
-    <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
-        <label name="search">Enter your postcode...</label>
-        <input
-          type="search"
-          name="search"
-          onChange={onChange}
-          value={userInput}
-        ></input>
-        <button type="submit" onClick={handleSearch}>
-          Submit
-        </button>
-      </form>
-      <p>Find your nearest textile recycle point</p>
+            <p>Find your nearest textile recycle point</p>
 
-      {data
-        ? data.map((item, index) => {
-            if (index < listCount)
-              return (
-                <ul key={item.id}>
-                  <li>
-                    {item.name} <br />
-                    {item.address} <br />
-                    {item.distance} <br />
-                  </li>
-                </ul>
-              );
-          })
-        : ""}
+            {data
+                ? data.map((item, index) => {
+                    if (index < listCount)
+                        return (
+                            <ul key={item.id}>
+                                <li>
+                                    {item.name} <br />
+                                    {item.address} <br />
+                                    {item.distance} <br />
+                                </li>
+                            </ul>
+                        );
+                })
+                : ""}
 
-      {error ? error : ""}
-    </>
-  );
+            {error ? error : ""}
+        </>
+    );
 }
