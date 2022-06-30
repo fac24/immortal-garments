22cd64e5c5fc6c6a00d98312d433c85a6ee25a55
sonianb <sonia.botsarova@gmail.com>
2022-06-30 15:01:01 +0100

    basic style navbar
    Co-authored-by: Alex <lascellesabercrombie@users.noreply.github.com>

--- components/Navlink.js
+++ components/Navlink.js
@@ -1,12 +1,11 @@
 import Link from "next/link";
 
-
 export default function Navlink(props) {
-    return (
-        <Link href={props.href}>
-            <a>{props.children}</a>
-        </Link>
-
-    )
+  return (
+    <Link href={props.href}>
+      <a className="hover:underline decoration-coral underline-offset-4">
+        {props.children}
+      </a>
+    </Link>
+  );
 }
-
