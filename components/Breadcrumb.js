794f3e58700a7968c70dbd6cb8f013746e28a1fa
sonianb <sonia.botsarova@gmail.com>
2022-06-30 11:13:47 +0100

    replace hard-coded links with useRouter method
    Co-authored-by: Hussain Jawad <HusJAW@users.noreply.github.com>

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -1,44 +1,36 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
-const links = [
-  { href: "/", label: "Home", step: 1 },
-  { href: "/clothes-condition", label: "Clothes condition", step: 2 },
-  { href: "/mend-options", label: "Mend options", step: 2 },
-  { href: "/donate", label: "Donate", step: 3 },
-  { href: "/recycle", label: "Recycle", step: 3 },
-  { href: "/diy", label: "DIY", step: 3 },
-  { href: "/tailors", label: "Tailors", step: 3 },
-].map((link) => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
-});
+const Breadcrumb = () => {
+  const router = useRouter();
+  const items = router.asPath.split("/");
 
-const Breadcrumb = ({ page, currentStep }) => (
-  <nav>
-    <ul>
-      <li></li>
-      {links
-        .filter(
-          ({ label, step }) =>
-            step < currentStep || (step === currentStep && page === label)
-        )
-        .map(({ key, href, label }) => {
-          if (page === label) {
-            return (
-              <li key={key}>
-                <span>{label}</span>
-              </li>
-            );
-          } else {
-            return (
-              <li key={key}>
-                <a href={href}>{label}</a>
-              </li>
-            );
-          }
-        })}
-    </ul>
-  </nav>
-);
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>home</a>
+          </Link>
+        </li>
+        {items.map((item, index) => (
+          <li>
+            {item.replaceAll("-", " ")}/
+            {/* <Link
+              href={{
+                pathname: `${item === items[1]}`
+                  ? `/${items[1]}`
+                  : `/${items[1]}/${item}`,
+              }}
+            >
+              <a></a>
+            </Link>{" "} */}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Breadcrumb;
