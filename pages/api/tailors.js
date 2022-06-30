a8298bf5eff8755584c541d39ded7cc711af583c
Maria <maria.louisa.castro@gmail.com>
2022-06-30 16:52:29 +0100

    add files

--- pages/api/tailors.js
+++ pages/api/tailors.js
@@ -1,20 +1,19 @@
-const url =
-"https://api.yelp.com/v3/businesses/search?latitude=51.5552&longitude=-0.14544&categories=sewingalterations&sort_by=distance";
-const options = {
-headers: {
-  Authorization:
-    "Bearer WV7hH1ykcn-jX5FFyoy3y5NKhZj6Y171UYimFZVmJJd0FsUsha1sTs9KeCugAZS8sEFBBSb_Ur1pakHS85RmFygzgtLJNyp93lMiceeCmjX6ZAXS_s5GiqgLLHO9YnYx",
-},
-};
-
-
 export default async function handlerTailors(req, res) {
-    
-  const result =  await fetch(url, options);
-  if (!result.ok) {
-    console.error(result.error);
-    return;
-  }
-  res.status(200).json(result)
+  const postcode = req.query.input;
 
-  }
+  const url = `https://api.yelp.com/v3/businesses/search?location=${postcode}&categories=sewingalterations&sort_by=distance`;
+
+  const options = {
+    headers: {
+      Authorization:
+        "Bearer WV7hH1ykcn-jX5FFyoy3y5NKhZj6Y171UYimFZVmJJd0FsUsha1sTs9KeCugAZS8sEFBBSb_Ur1pakHS85RmFygzgtLJNyp93lMiceeCmjX6ZAXS_s5GiqgLLHO9YnYx",
+    },
+  };
+
+  const result = await fetch(url, options);
+  const data = await result.json();
+  console.log(data)
+  res.status(200).json(data);  
+  // tailorsData = data;
+  // res.redirect("/mend-options/tailors");  
+}
--- pages/mend-options/tailors.js
+++ pages/mend-options/tailors.js
@@ -1,5 +1,25 @@
+import React, { useState, useEffect } from "react";
 import Breadcrumb from "../../components/Breadcrumb";
 
-export default function Tailors() {
-  return <Breadcrumb />;
+async function fetchData(postcode) {
+const response = await fetch(`http://localhost:3000/api/tailors?input=${postcode}`)
+const data = await response.json();
+console.log(data)
 }
+
+export default function Tailors() {
+  const [postcode, setPostcode] = useState("")
+
+    return (
+      <>
+        <Breadcrumb />
+        <form onSubmit={e => {
+          e.preventDefault()
+          fetchData(postcode)
+        }}>
+          <input type="text" name="input" onChange={(e)=>setPostcode(e.target.value)}/>
+          <button type="submit">search</button>
+        </form>
+      </>
+    );
+  }
