fb1c21b9de91c47e15fac0316ba4136407eccbdc
Alex Grafen <alex.grafen@gmail.com>
2022-06-30 11:27:28 +0100

    reset tailors content

--- pages/tailors.js
+++ pages/tailors.js
@@ -1,27 +1,3 @@
-import SearchPostcode from "../components/SearchPostcode";
-import { useState } from "react";
-
 export default function Tailors() {
-    const [businessInfo, setBusinessInfo] = useState(null);
-    async function handleSearch() {
-        const result = await fetch(`https://api.yelp.com/v3/businesses/search`);
-        console.log(result);
-        if (!result.ok) {
-            setBusinessInfo(null);
-            setError(`Oops, something went wrong: ${result.status}.`);
-            return;
-        }
-
-        setError(null);
-        const newInfo = await result.json();
-        setBusinessInfo(newInfo);
-    }
-
-    return (<>
-        <SearchPostcode
-            onChange={onChange}
-            value={userInput}
-            handleSearch={handleSearch}
-        />
-    </>);
+    return (<p>test</p>);
 }
