4a78f6df7a751d737a72f98aee7ec5eef512008a
sonianb <sonia.botsarova@gmail.com>
2022-07-04 14:42:50 +0100

    put h1 inside main

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,23 @@ import repair from "../public/images/repair.png";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-wrap gap-3">
-      <section>
-        <ChoiceButton
-          href="/clothes-condition"
-          src={donateclothes}
-          alt=""
-        ></ChoiceButton>
-        <Link href="/clothes-condition">Donate And Recycle</Link>
-      </section>
+    <>
+      <h1 className="text-2xl pb-4">Immortal Garments</h1>
+      <div className="flex flex-col flex-wrap gap-3">
+        <section>
+          <ChoiceButton
+            href="/clothes-condition"
+            src={donateclothes}
+            alt=""
+          ></ChoiceButton>
+          <Link href="/clothes-condition">Donate And Recycle</Link>
+        </section>
 
-      <section>
-        <ChoiceButton href="/mend-options" src={repair}></ChoiceButton>
-        <Link href="/mend-options">Care And Repair</Link>
-      </section>
-    </div>
+        <section>
+          <ChoiceButton href="/mend-options" src={repair}></ChoiceButton>
+          <Link href="/mend-options">Care And Repair</Link>
+        </section>
+      </div>
+    </>
   );
 }
