966001405db5cf4c1f05c63ca38c0bf8375aab34
Maria <maria.louisa.castro@gmail.com>
2022-07-04 11:54:33 +0100

    Modularize serch API for reuse

---
+++ components/SearchAPI.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+export default function SearchAPI({ setTailorsData, setError }) {
+  const [postcode, setPostcode] = useState("");
+
+  async function fetchData(x) {
+    const result = await fetch(`../api/tailors?input=${x}`);
+    const data = await result.json();
+
+    // Error handling incase of a failed fetch
+    if (data.error) {
+      setTailorsData(null);
+      setError(data.error.description);
+    } else {
+      setError(null);
+      setTailorsData(data);
+    }
+  }
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        fetchData(postcode);
+      }}
+      className="max-w-xs flex flex-col my-4 gap-y-3"
+    >
+      <label name="search" className="text-gray-800">
+        Search a postcode
+      </label>
+      <input
+        type="text"
+        name="input"
+        onChange={(e) => setPostcode(e.target.value)}
+        className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-full"
+      />
+      <button
+        type="submit"
+        className="bg-darkGreen hover:bg-[#51ae68] text-gray-900 font-semibold py-2 px-4 border border-gray-400 rounded shadow flex-auto"
+      >
+        Search
+      </button>
+    </form>
+  );
+}
--- pages/mend-options/tailors.js
+++ pages/mend-options/tailors.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import Breadcrumb from "../../components/Breadcrumb";
 //import Image from "next/image";
+import SearchAPI from "../../components/SearchAPI";
 
 export default function Tailors() {
-  const [postcode, setPostcode] = useState("");
   const [tailorsData, setTailorsData] = useState(null);
   const [error, setError] = useState(null);
 
@@ -24,51 +24,39 @@ export default function Tailors() {
   return (
     <>
       <Breadcrumb />
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          fetchData(postcode);
-        }}
-      >
-        <input
-          type="text"
-          name="input"
-          onChange={(e) => setPostcode(e.target.value)}
-        />
-        <button type="submit">search</button>
-        <div>
-          {error ? error : ""}
-          {tailorsData
-            ? tailorsData.businesses.map((place) => {
-                return (
-                  <div key={place.id} className="flex m-10">
-                    <section>
-                      <img
-                        src={
-                          place.image_url.length > 1
-                            ? place.image_url
-                            : "https://thumbs.dreamstime.com/z/tailor-made-icon-vector-illustration-tailor-made-icon-vector-illustration-white-background-119861864.jpg"
-                        }
-                        alt={place.name}
-                        className="h-20"
-                      />
-                    </section>
+      <SearchAPI setTailorsData={setTailorsData} setError={setError} />
+      <div>
+        {error ? error : ""}
+        {tailorsData
+          ? tailorsData.businesses.map((place) => {
+              return (
+                <div key={place.id} className="flex m-10">
+                  <section>
+                    <img
+                      src={
+                        place.image_url.length > 1
+                          ? place.image_url
+                          : "https://thumbs.dreamstime.com/z/tailor-made-icon-vector-illustration-tailor-made-icon-vector-illustration-white-background-119861864.jpg"
+                      }
+                      alt={place.name}
+                      className="h-20"
+                    />
+                  </section>
 
-                    <section>
-                      <p>{place.name}</p>
-                      <p>
-                        {place.location.address1}, {place.location.city},{" "}
-                        {place.location.zip_code}
-                      </p>
-                      <p>{place.phone}</p>
-                      <p>{place.price}</p>
-                    </section>
-                  </div>
-                );
-              })
-            : ""}
-        </div>
-      </form>
+                  <section>
+                    <p>{place.name}</p>
+                    <p>
+                      {place.location.address1}, {place.location.city},{" "}
+                      {place.location.zip_code}
+                    </p>
+                    <p>{place.phone}</p>
+                    <p>{place.price}</p>
+                  </section>
+                </div>
+              );
+            })
+          : ""}
+      </div>
     </>
   );
 }
