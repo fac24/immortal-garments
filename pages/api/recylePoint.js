7da27fb2c235a07d6e77120181dd30d669074940
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-07-04 14:27:36 +0100

    update

--- pages/api/recylePoint.js
+++ pages/api/recylePoint.js
@@ -5,8 +5,7 @@ export default async function handlerRecylePoint(req, res) {
   // const url =
   try {
     const result = await fetch(
-      process.env.RECYCLE_API +
-        `${postcode}?limit=30&radius=25&materials%5B%5D=57`
+      `https://rl.recyclenow.com/widget/londonrecycles.co.uk/locations/${postcode}?limit=30&radius=25&materials%5B%5D=57`
     );
     const data = await result.json();
     res.status(200).json(data);
