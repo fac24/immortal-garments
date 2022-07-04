00ca50388a5cffaf82668e98971bcafbfd225858
Alex Grafen <alex.grafen@gmail.com>
2022-07-04 16:51:22 +0100

    move some usestates to a higher level

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const [userPosition, setUserPosition] = useState(null);
+  const [listCount, setListCount] = useState(7);
   return <>
     <Layout>
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        userPosition={userPosition}
+        setUserPosition={setUserPosition}
+        listCount={listCount}
+        setListCount={setListCount} />
     </Layout>
   </>
 }
