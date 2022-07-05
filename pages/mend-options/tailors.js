0abc56f7c8ad5630ca754fcfdec8df27b161980b
sonianb <sonia.botsarova@gmail.com>
2022-07-05 11:00:29 +0100

    set progress to 100 when user finds tailors results

--- pages/mend-options/tailors.js
+++ pages/mend-options/tailors.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Breadcrumb from "../../components/Breadcrumb";
 import SearchAPI from "../../components/SearchAPI";
 import SearchResults from "../../components/SearchResults";
+import ProgressBar from "../../components/ProgressBar";
 
 export default function Tailors() {
   const [tailorsData, setTailorsData] = useState(null);
   const [error, setError] = useState(null);
+  const [progress, setProgress] = useState(75);
+
+  useEffect(() => {
+    if (tailorsData) {
+      setProgress(100);
+    } else {
+      setProgress(75);
+    }
+  }, [tailorsData]);
 
   return (
     <>
       <Breadcrumb />
+      <section>
+        <div>
+          <ProgressBar completed={progress} aria-valuenow={progress} />
+        </div>
+      </section>
+
       <h2 className="text-xl py-3">Tailors</h2>
       <p>
         Find your nearest tailors, dry cleaners or seamstress for your clothes
