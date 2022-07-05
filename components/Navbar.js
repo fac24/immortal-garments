23df1a1537fd7b880076592e0dc9d1452646b5f1
Alex Grafen <alex.grafen@gmail.com>
2022-07-05 12:35:23 +0100

    begin styling hamburger menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,6 +3,13 @@ import Navlink from "./Navlink";
 export default function Navbar() {
   return (
     <nav className="flex justify-center gap-5 text-2xl font-semibold my-3">
+      <input type="checkbox" id="hamburger"></input>
+
+      <label htmlFor="hamburger" className="span-container">
+        <span className="w-35 h-3 bg-black m-5"></span>
+        <span></span>
+        <span></span>
+      </label>
       <Navlink href="/">Home</Navlink>
       <Navlink href="/clothes-condition/donate">Donate</Navlink>
       <Navlink href="/clothes-condition/recycle">Recycle</Navlink>
