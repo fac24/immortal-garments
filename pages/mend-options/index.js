68258fdc0663bbfba692159ffc935cf0e8c71f12
sonianb <sonia.botsarova@gmail.com>
2022-07-05 10:59:54 +0100

    set progress to 75 when user is on mend options page

--- pages/mend-options/index.js
+++ pages/mend-options/index.js
@@ -4,12 +4,19 @@ import mendByMyself from "../../public/images/mendbymyself.png";
 import repair from "../../public/images/repair.png";
 import Breadcrumb from "../../components/Breadcrumb";
 import React, { useState } from "react";
+import ProgressBar from "../../components/ProgressBar";
 
 export default function MendOptions() {
   return (
-    <main>
+    <>
       <section>
         <Breadcrumb />
+        <section>
+          <div>
+            <ProgressBar completed={75} aria-valuenow={75} />
+          </div>
+        </section>
+
         <h2 className="py-3 text-xl">Mend...</h2>
         <ChoiceButton
           href="/mend-options/diy"
@@ -24,6 +31,6 @@ export default function MendOptions() {
         ></ChoiceButton>
         <Link href="/mend-options/tailors">by a professional</Link>
       </section>
-    </main>
+    </>
   );
 }
