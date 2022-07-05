d26ec6f6c0e4307f5e9947e794a1243656e3ff08
sonianb <sonia.botsarova@gmail.com>
2022-07-05 16:15:55 +0100

    remove progress bar label

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -3,30 +3,29 @@ import React from "react";
 export default function ProgressBar({ completed }) {
   const fillerStyles = {
     width: `${completed}%`,
-    borderRadius: "inherit",
-    textAlign: "right",
     transition: "width 1s ease-in-out",
   };
 
   const labelStyles = {
-    left: `calc(${completed}% - 5px)`,
+    left: `calc(${completed}% - 8px)`,
   };
 
   return (
-    <div className="relative h-2 rounded-full bg-[#e0e0de]">
-      <div className="h-full bg-lightGreen" style={fillerStyles}>
-        <span
-          style={labelStyles}
-          role="progressbar"
-          aria-valuenow={completed}
-          aria-valuemin="0"
-          aria-valuemax="100"
-        ></span>
+    <>
+      <div className="relative h-2 rounded-full bg-[#e0e0de] w-[16rem]">
+        <div
+          className="h-full bg-lightGreen rounded-full text-right"
+          style={fillerStyles}
+        >
+          <span
+            style={labelStyles}
+            role="progressbar"
+            aria-valuenow={completed}
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></span>
+        </div>
       </div>
-      <span
-        className="absolute text-sm font-bold pt-1"
-        style={labelStyles}
-      >{`${completed}%`}</span>
-    </div>
+    </>
   );
 }
