a668e99a9b2b15c3fd35679edb38c9ef1d4e958c
Alex Grafen <alex.grafen@gmail.com>
2022-07-05 10:54:26 +0100

    more graph changes

--- pages/learning.js
+++ pages/learning.js
@@ -72,23 +72,6 @@ function BarChart() {
                         // .text(data.y1)
                     );
             // yAxis = g => g
-            const tooltip = d3.select("body")
-                .append("div")
-                .style("position", "absolute")
-                .style("z-index", "10")
-                .style("visibility", "hidden")
-                .style("background", "#000")
-                .text("a simple tooltip");
-
-            d3.select("body")
-                .selectAll("div")
-                .data(data)
-                .enter().append("div")
-                .style("width", function (d) { return x(d) + "50" + "px"; })
-                .text(function (d) { return d; })
-                .on("mouseover", function (d) { tooltip.text(d); return tooltip.style("visibility", "visible"); })
-                .on("mousemove", function () { return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"); })
-                .on("mouseout", function () { return tooltip.style("visibility", "hidden"); });
 
             svg.select(".x-axis").call(xAxis);
             svg.select(".y-axis").call(y1Axis);
