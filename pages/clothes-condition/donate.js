36c80022305898c7374f59c7ae9373bf706762e4
sonianb <sonia.botsarova@gmail.com>
2022-07-05 10:56:22 +0100

    set progress to 75 when user searches for donation points

--- pages/clothes-condition/donate.js
+++ pages/clothes-condition/donate.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Breadcrumb from "../../components/Breadcrumb";
 import SearchAPI from "../../components/SearchAPI";
 import SearchResults from "../../components/SearchResults";
+import ProgressBar from "../../components/ProgressBar";
 
 export default function Donate() {
   const [tailorsData, setTailorsData] = useState(null);
+  const [progress, setProgress] = useState(75);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (tailorsData) {
+      setProgress(100);
+    } else {
+      setProgress(75);
+    }
+  }, [tailorsData]);
 
   return (
     <>
       <Breadcrumb />
-      <h2 className="text-xl py-3">Donate</h2>
+      <section>
+        <div>
+          <ProgressBar completed={progress} aria-valuenow={progress} />
+        </div>
+      </section>
 
+      <h2 className="text-xl py-3">Donate</h2>
       <p>
         Find your nearest clothing donation bank to donate your unwanted
         clothes.
