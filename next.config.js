52a03fd61aa1273d83d1536dad23ee6466b71662
sonianb <sonia.botsarova@gmail.com>
2022-07-07 14:46:25 +0100

    disable webpack 5

--- next.config.js
+++ next.config.js
@@ -5,11 +5,14 @@ const nextConfig = {
 
 module.exports = {
   webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      loader: require.resolve("@svgr/webpack"),
     });
-
     return config;
   },
   i18n: {
